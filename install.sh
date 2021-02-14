601ab7fa77cfd3e046c36900f3d930568dc336ab
victor-gp <victor.gp.dev@gmail.com>
2021-02-14 15:58:15 +0100

    implement dotbot installation of git, zsh

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-set -eu
+# <configuration>
 
 DOTBOT_VERSION="v1.18.0"
+TOPICS=(
+    git
+    vscode
+    zsh
+)
+
+# </configuration>
+
+set -eu
+
 DOTBOT_DIR="dotbot"
 DOTBOT_BIN="bin/dotbot"
 
@@ -12,4 +22,7 @@ git -C "$DOTBOT_DIR" checkout --quiet "tags/${DOTBOT_VERSION}"
 git -C "$DOTBOT_DIR" submodule --quiet update --init --recursive
 echo "Checkout successful" >&2
 
-"${DOTBOT_DIR}/${DOTBOT_BIN}" -c vscode/install.config.yaml
+for topic in "${TOPICS[@]}"; do
+    echo "Dotbot-installing topic ‘$topic’..."
+    "${DOTBOT_DIR}/${DOTBOT_BIN}" -c "$topic"/install.config.yaml
+done
