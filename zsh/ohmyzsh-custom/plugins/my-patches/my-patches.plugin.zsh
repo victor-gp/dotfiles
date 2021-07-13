33c84ef6144aa7ee83360c346fcf9283a268f78c
Víctor González Prieto <victor.gp.dev@gmail.com>
2021-07-14 01:33:04 +0200

    zsh: reoriganize syntax highlight styles
    Mainly moving things around (group by style, then sort alphabetically)
    and removing a couple of styles that I don't really use.

--- zsh/ohmyzsh-custom/plugins/my-patches/my-patches.plugin.zsh
+++ zsh/ohmyzsh-custom/plugins/my-patches/my-patches.plugin.zsh
@@ -6,24 +6,22 @@ ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 
 ZSH_HIGHLIGHT_STYLES[default]=none
 ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=red
-ZSH_HIGHLIGHT_STYLES[alias]=none
-ZSH_HIGHLIGHT_STYLES[builtin]=none
-ZSH_HIGHLIGHT_STYLES[function]=none
-ZSH_HIGHLIGHT_STYLES[command]=none
-ZSH_HIGHLIGHT_STYLES[precommand]=fg=yellow
 ZSH_HIGHLIGHT_STYLES[commandseparator]=fg=yellow
+ZSH_HIGHLIGHT_STYLES[precommand]=fg=yellow
+ZSH_HIGHLIGHT_STYLES[redirection]=fg=yellow
 ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=yellow
-ZSH_HIGHLIGHT_STYLES[hashed-command]=none
-ZSH_HIGHLIGHT_STYLES[autodirectory]=none
-ZSH_HIGHLIGHT_STYLES[path]=none
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=magenta
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=magenta
+ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=cyan
+ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=cyan
 ZSH_HIGHLIGHT_STYLES[globbing]=fg=cyan
 ZSH_HIGHLIGHT_STYLES[history-expansion]=fg=cyan
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=magenta
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=magenta
-ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=cyan
-ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=cyan
-ZSH_HIGHLIGHT_STYLES[assign]=none
-ZSH_HIGHLIGHT_STYLES[redirection]=fg=yellow
+ZSH_HIGHLIGHT_STYLES[alias]=none
+ZSH_HIGHLIGHT_STYLES[autodirectory]=none
+ZSH_HIGHLIGHT_STYLES[builtin]=none
+ZSH_HIGHLIGHT_STYLES[command]=none
+ZSH_HIGHLIGHT_STYLES[function]=none
+ZSH_HIGHLIGHT_STYLES[path]=none
 
 ZSH_HIGHLIGHT_STYLES[bracket-error]=fg=red,bold
 ZSH_HIGHLIGHT_STYLES[bracket-level-1]=none
