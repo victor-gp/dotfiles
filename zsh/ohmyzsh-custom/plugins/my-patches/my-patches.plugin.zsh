83b7055c0c5ee5511e39d070ea7c35595d521011
Víctor González Prieto <victor.gp.dev@gmail.com>
2021-07-14 00:53:11 +0200

    zsh: improve syntax highlighting
    Mainly prevents the gaudy highlighting of brackets and parens that comes
    by default. It unnecessarily overshadows the syntax of other elements,
    like variable interpolation inside a string.
    
    Then also makes a bunch of tweaks to other syntactic elements that I
    want highlighted.

--- zsh/ohmyzsh-custom/plugins/my-patches/my-patches.plugin.zsh
+++ zsh/ohmyzsh-custom/plugins/my-patches/my-patches.plugin.zsh
@@ -10,8 +10,9 @@ ZSH_HIGHLIGHT_STYLES[alias]=none
 ZSH_HIGHLIGHT_STYLES[builtin]=none
 ZSH_HIGHLIGHT_STYLES[function]=none
 ZSH_HIGHLIGHT_STYLES[command]=none
-ZSH_HIGHLIGHT_STYLES[precommand]=none
+ZSH_HIGHLIGHT_STYLES[precommand]=fg=yellow
 ZSH_HIGHLIGHT_STYLES[commandseparator]=fg=yellow
+ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=yellow
 ZSH_HIGHLIGHT_STYLES[hashed-command]=none
 ZSH_HIGHLIGHT_STYLES[autodirectory]=none
 ZSH_HIGHLIGHT_STYLES[path]=none
@@ -19,9 +20,18 @@ ZSH_HIGHLIGHT_STYLES[globbing]=fg=cyan
 ZSH_HIGHLIGHT_STYLES[history-expansion]=fg=cyan
 ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=magenta
 ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=magenta
-ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=magenta
-ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=magenta
+ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=cyan
+ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=cyan
 ZSH_HIGHLIGHT_STYLES[assign]=none
+ZSH_HIGHLIGHT_STYLES[redirection]=fg=yellow
+
+ZSH_HIGHLIGHT_STYLES[bracket-error]=fg=red,bold
+ZSH_HIGHLIGHT_STYLES[bracket-level-1]=none
+ZSH_HIGHLIGHT_STYLES[bracket-level-2]=none
+ZSH_HIGHLIGHT_STYLES[bracket-level-3]=none
+ZSH_HIGHLIGHT_STYLES[bracket-level-4]=none
+ZSH_HIGHLIGHT_STYLES[bracket-level-5]=none
+ZSH_HIGHLIGHT_STYLES[cursor-matchingbracket]=bold
 
 typeset -A ZSH_HIGHLIGHT_PATTERNS
 # do not disturb on the first token
