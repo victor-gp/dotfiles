504a2b7d64b31c5b3beb1d1e45cb5bdd167d8d6e
Víctor González Prieto <victor.gp.dev@gmail.com>
2021-10-29 10:31:26 +0200

    zsh: misc improvements to functions/lnlog
    Print all messages to stderr, return error code 2 on invalid arguments,
    as suggested here: https://stackoverflow.com/a/18568726
    
    Missed a chance to name help() helpless...

--- zsh/ohmyzsh-custom/functions.zsh
+++ zsh/ohmyzsh-custom/functions.zsh
@@ -11,39 +11,40 @@ lnlog() {
     log_shortpath="${log_path/*dev-logs/dev-logs}"
 
     if [[ -z "$log_path" ]]; then
-        echo 'lnlog error: no path given'
-        echo 'lnlog usage example: lnlog $DEVLOGS_PATH/personal/dotfiles.md'
-        return 1
+        >&2 echo 'lnlog error: no path given'
+        >&2 echo 'usage example: lnlog $DEVLOGS_PATH/personal/dotfiles.md'
+        return 2
     elif [[ ! "$log_path" =~ ^.*dev-logs/.+$ ]]; then
-        echo 'lnlog error: path must be a file within dev-logs/'
-        echo 'lnlog usage example: lnlog $DEVLOGS_PATH/personal/dotfiles.md'
-        return 1
+        >&2 echo 'lnlog error: path must be a file within dev-logs/'
+        >&2 echo 'usage example: lnlog $DEVLOGS_PATH/personal/dotfiles.md'
+        return 2
     elif [[ ! -d "$log_dirname/" ]]; then
-        echo "lnlog error: no '$log_dirname' directory"
-        return 1
+        >&2 echo "lnlog error: no '$log_dirname' directory"
+        return 2
     elif [[ -h "$notes_basename" ]]; then
-        echo "lnlog error: '$notes_basename' is already a link"
+        >&2 echo "lnlog error: '$notes_basename' is already a link"
+        # so better do things manually
         return 1
     elif [[ -f "$notes_basename" ]]; then
         if [[ ! -a "$log_path" ]]; then
             # notes exist, but they're not kept in dev-logs yet
             mv -n "$notes_basename" "$log_path"
             ln -s "$log_path" "$notes_basename"
-            echo "lnlog: moved '$notes_basename' to '$log_shortpath', then linked it here"
+            >&2 echo "lnlog: moved '$notes_basename' to '$log_shortpath', then linked it here"
         else
             # both notes and dev-log exist
-            echo "lnlog error: cannot clobber existing '$log_shortpath' with '$notes_basename'"
+            >&2 echo "lnlog error: cannot clobber existing '$log_shortpath' with '$notes_basename'"
             return 1
         fi
     elif [[ -f "$log_path" ]]; then
         # dev-log already exists, but it's not linked here yet
         ln -s "$log_path" "$notes_basename"
-        echo "lnlog: linked existing '$log_shortpath' on '$notes_basename'"
+        >&2 echo "lnlog: linked existing '$log_shortpath' on '$notes_basename'"
     else
         # brand new dev-log
         touch "$log_path"
         ln -s "$log_path" "$notes_basename"
-        echo "lnlog: created '$log_shortpath', linked it on '$notes_basename'"
+        >&2 echo "lnlog: created '$log_shortpath', linked it on '$notes_basename'"
     fi
 }
 
@@ -52,7 +53,7 @@ help() {
     if [ "$#" -eq 0 ]; then
         >&2 echo "$0: no argument provided"
         >&2 echo "usage: $0 COMMAND [SUBCOMMAND]..."
-	    return 2
+        return 2
     fi
 
     "$@" --help | less -R
