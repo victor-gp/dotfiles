b5c8bac0fbfb88e02bef8402638eb668a06f2f79
Víctor González Prieto <victor.gp.dev@gmail.com>
2022-03-05 16:37:29 +0100

    zsh: use subshell functions
    I had this sitting in the backlog for a while. Inspired by this reddit
    disc [1] about this blog [2].
    
    Using subshell functions means I can write my utils with the same
    assumptions as in plain old bash scripts. So exits instead of returns,
    no need to make variables local (which seems to be itself faulty),
    scoping works infinitely better (lexical scoping), etc.
    
    [1] https://www.reddit.com/r/programming/comments/qjnzmn/underwhelmed_by_bash_functions_maybe_youre_using/
    [2] https://cuddly-octo-palm-tree.com/posts/2021-10-31-better-bash-functions/

--- zsh/utils.sh
+++ zsh/utils.sh
@@ -18,31 +18,30 @@ zle -N vi-yank-pbcopy
 bindkey -M vicmd 'y' vi-yank-pbcopy
 
 # usage example: lnlog DEVLOGS_DIR/personal/dotfiles.md
-lnlog() {
-    local log_path="$1"
-    local log_dirname
+lnlog() (
+    log_path="$1"
     log_dirname=$(dirname "$log_path")
-    local log_extension="${log_path/*./.}"
+    log_extension="${log_path/*./.}"
     # corner case: no file extension
     [[ "$log_extension" == "$log_path" ]] && log_extension=''
-    local notes_basename="vgp-notes$log_extension"
-    local log_shortpath="${log_path/*dev-logs/dev-logs}"
+    notes_basename="vgp-notes$log_extension"
+    log_shortpath="${log_path/*dev-logs/dev-logs}"
 
     if [[ -z "$log_path" ]]; then
         >&2 echo 'lnlog error: no path given'
         >&2 echo 'usage example: lnlog DEVLOGS_DIR/personal/dotfiles.md'
-        return 2
+        exit 2
     elif [[ ! "$log_path" =~ .*dev-logs/.+$ ]]; then
         >&2 echo 'lnlog error: path must be a file within dev-logs/'
         >&2 echo 'usage example: lnlog DEVLOGS_DIR/personal/dotfiles.md'
-        return 2
+        exit 2
     elif [[ ! -d "$log_dirname/" ]]; then
         >&2 echo "lnlog error: no '$log_dirname' directory"
-        return 2
+        exit 2
     elif [[ -h "$notes_basename" ]]; then
         >&2 echo "lnlog error: '$notes_basename' is already a link"
         # so better do things manually
-        return 1
+        exit 1
     elif [[ -f "$notes_basename" ]]; then
         if [[ ! -a "$log_path" ]]; then
             # notes exist, but they're not kept in dev-logs yet
@@ -52,7 +51,7 @@ lnlog() {
         else
             # both notes and dev-log exist
             >&2 echo "lnlog error: cannot clobber existing '$log_shortpath' with '$notes_basename'"
-            return 1
+            exit 1
         fi
     elif [[ -f "$log_path" ]]; then
         # dev-log already exists, but it's not linked here yet
@@ -64,18 +63,18 @@ lnlog() {
         ln -s "$log_path" "$notes_basename"
         >&2 echo "lnlog: created '$log_shortpath', linked it on '$notes_basename'"
     fi
-}
+)
 
 # paginated & colorized --help
-help() {
+help() (
     if [ "$#" -eq 0 ]; then
-        >&2 echo "$0: no argument provided"
+        >&2 echo "$0 error: no argument provided"
         >&2 echo "usage: $0 COMMAND [SUBCOMMAND]..."
-        return 2
+        exit 2
     fi
 
     "$@" --help | bat --no-config -p --language=cmd-help --pager='less -R'
 
     #todo: custom16 theming for cmd-help scopes
     #todo: on error, try "$@ -h", then "{first token} help {the rest}"
-}
+)
